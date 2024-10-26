f427b7c8c08f5f0d5d24593c84abf53fec73a731
HomeCpu <zovl5546@naver.com>
2024-10-26 21:05:19 +0900

    충돌테스트

--- src/main/java/com/sandbox/Sprinkler/controller/TodoController.java
+++ src/main/java/com/sandbox/Sprinkler/controller/TodoController.java
@@ -2,8 +2,8 @@ package com.sandbox.Sprinkler.controller;
 
 public class TodoController {
     public void strnig() {
-        System.out.println("asdfa");
-        System.out.println("hhelloo wordld");
+        System.out.println("바박바바가바바바바밥밥가박박");
+        System.out.println("박바바박바바박바바박바바");
         return;
     }
 }
