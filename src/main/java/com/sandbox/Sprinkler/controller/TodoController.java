69b2bed633c58444a2da1642555a3cc617a68237
coolOlive <galilee155@naver.com>
2024-10-26 21:08:51 +0900

    test: 충돌돌

--- src/main/java/com/sandbox/Sprinkler/controller/TodoController.java
+++ src/main/java/com/sandbox/Sprinkler/controller/TodoController.java
@@ -2,7 +2,7 @@ package com.sandbox.Sprinkler.controller;
 
 public class TodoController {
     public void strnig() {
-        System.out.println("asdfa");
+        System.out.println("여기 수정할꺼");
         System.out.println("hhelloo wordld");
         return;
     }
--- src/main/java/com/sandbox/Sprinkler/service/CRUDService.java
+++ src/main/java/com/sandbox/Sprinkler/service/CRUDService.java
@@ -2,6 +2,7 @@ package com.sandbox.Sprinkler.service;
 
 public class CRUDService {
     public static void main(String[] args) {
+
         System.out.println("service");
     }
 }
