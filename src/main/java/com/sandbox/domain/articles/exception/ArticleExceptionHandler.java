80f7e9aff29dba6249c5db05a75cd5d1ab8f798f
nangdi <zovl5546@naver.com>
2024-11-07 23:24:20 +0900

    refactor : 에러 처리 추가

--- src/main/java/com/sandbox/domain/articles/exception/ArticleExceptionHandler.java
+++ src/main/java/com/sandbox/domain/articles/exception/ArticleExceptionHandler.java
@@ -1,4 +1,18 @@
 package com.sandbox.domain.articles.exception;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@Slf4j
 public class ArticleExceptionHandler {
+
+    @ExceptionHandler(ErrorArticleResp.class)
+    public ResponseEntity<ErrorArticleResp> handleErrorTodo(ErrorArticleResp ex){
+        log.error("ErrorArticleResp 발생 : {}", ex.getMessage(), ex);
+
+        return ResponseEntity.status(202).body(new ErrorArticleResp(ex.getMessage()));
+    }
 }
